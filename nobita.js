d315c3a8ab9170ad93228855cd685d33e2a6f474
Lam <lnhlam99@gmail.com>
2022-08-12 14:22:17 +0700

    fearture-03-fearture

--- nobita.js
+++ nobita.js
@@ -1,18 +1,26 @@
 function upArrowPressd() {
     let element = document.getElementById('nobita');
-    element.style.top = parseInt(element.style.top) - 5 + 'px';
+    if (parseInt(element.style.top) > 0) {
+        element.style.top = parseInt(element.style.top) - 5 + 'px';
+    }
 }
 function downArrowPressed() {
     var element = document.getElementById("nobita");
-    element.style.top = parseInt(element.style.top) + 5 + 'px';
+    if (parseInt(element.style.top) < 600) {
+        element.style.top = parseInt(element.style.top) + 5 + 'px';
+    }
 }
 function  leftArrowPressed() {
     var element = document.getElementById("nobita");
-    element.style.left = parseInt(element.style.left) - 5 + 'px';
+    if (parseInt(element.style.left)>0) {
+        element.style.left = parseInt(element.style.left) - 5 + 'px';
+    }
 }
 function  rightArrowPressed() {
     var element = document.getElementById("nobita");
-    element.style.left = parseInt(element.style.left) +5 +'px'
+    if (parseInt(element.style.left) < 1400) {
+        element.style.left = parseInt(element.style.left) + 5 + 'px'
+    }
 }
 function  moveSelection(evt) {
     switch (evt.keyCode) {
